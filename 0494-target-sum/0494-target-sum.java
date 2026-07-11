b8d37fc768d335481a97dd747801ebb7ca16b65b
Tushar Singh <tusharsingh7705@gmail.com>
2026-07-11 11:00:02 +0530

    Time: 10 ms (54.22%), Space: 46.5 MB (31.03%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int helper(int []nums,int i,int sum,int target,int [][]dp){
+        if(i==nums.length){
+            if(sum==target){
+                return 1;
+            }
+            return 0;
+        }
+        int shifted=sum+1000;
+        if(dp[i][shifted]!=-1){
+            return dp[i][shifted];
+        }
+
+        int pick=helper(nums,i+1,sum+nums[i],target,dp);
+        int notpick=helper(nums,i+1,sum-nums[i],target,dp);
+
+        return dp[i][shifted] =pick+notpick;
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int n=nums.length;
+
+        int [][]dp=new int [n][2001];
+        for(int []it:dp){
+            Arrays.fill(it,-1);
+        }
+        return helper(nums,0,0,target,dp);
+        
+    }
+}
