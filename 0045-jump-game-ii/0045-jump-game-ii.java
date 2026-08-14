d2f01b6d94c293f32d5ac9a02dde486fbca26b5f
Tushar Singh <tusharsingh7705@gmail.com>
2026-08-14 20:55:55 +0530

    Time: 1 ms (99.77%), Space: 47.4 MB (38.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,83 +1,19 @@
 class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
 
-    int n;
-    int[] nums;
-    int[] dp;
+        for (int i = 0; i < nums.length - 1; i++) {
 
-    public int helper(int index) {
+            farthest = Math.max(farthest, i + nums[i]);
 
-        if (index >= n - 1) {
-            return 0; 
-        }
-
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        int min = Integer.MAX_VALUE;
-
-        for (int j = 1; j <= nums[index]; j++) {
-            if (index + j < n) {
-                int next = helper(index + j);
-                if (next != Integer.MAX_VALUE) {
-                    min = Math.min(min, 1 + next);
-                }
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
         }
 
-        dp[index] = min;
-        return min;
-    }
-
-    public int jump(int[] nums) {
-        this.n = nums.length;
-        this.nums = nums;
-        dp = new int[n];
-
-        for (int i = 0; i < n; i++) dp[i] = -1;
-
-        return helper(0);
+        return jumps;
     }
 }
-
-
-
-
-
-// class Solution {
-
-//     int n;
-//     int[] nums;
-//     int[][] dp;
-
-//     public int helper(int index, int jumps) {
-//         if (index >= n - 1) {
-//             return jumps;
-//         }
-//         if(dp[index][jumps]!=-1){
-//             return dp[index][jumps];
-//         }
-
-//         int min = Integer.MAX_VALUE;
-
-//         for (int i = 1; i <= nums[index]; i++) {
-//             min = Math.min(min, helper(index + i, jumps + 1));
-//         }
-//         dp[index][jumps]=min; 
-//         return min;
-//     }
-
-//     public int jump(int[] nums) {
-//         this.n = nums.length;
-//         this.nums = nums;
-//         dp=new int[n][n+1];
-
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<=n;j++){
-
-//                 dp[i][j]=-1;
-//             }
-//         }
-//         return helper(0, 0);
-//     }
-// }
